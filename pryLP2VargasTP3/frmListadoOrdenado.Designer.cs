3b72012bfd55e247e5d8fe16eb70ca706ea3756f
palomaottonello <palomaottonello@gmail.com>
2026-04-13 11:53:09 -0300

    Más cosas 13/04

---
+++ pryLP2VargasTP3/frmListadoOrdenado.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace pryLP2VargasTP3
+{
+    partial class frmListadoOrdenado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbConsulta = new System.Windows.Forms.GroupBox();
+            this.lblCampo = new System.Windows.Forms.Label();
+            this.lblmodo = new System.Windows.Forms.Label();
+            this.cmbCampo = new System.Windows.Forms.ComboBox();
+            this.cmbModo = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LimitedeCredito = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Deuda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.grbConsulta.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbConsulta
+            // 
+            this.grbConsulta.Controls.Add(this.dataGridView1);
+            this.grbConsulta.Controls.Add(this.cmbModo);
+            this.grbConsulta.Controls.Add(this.cmbCampo);
+            this.grbConsulta.Controls.Add(this.lblmodo);
+            this.grbConsulta.Controls.Add(this.lblCampo);
+            this.grbConsulta.Location = new System.Drawing.Point(34, 13);
+            this.grbConsulta.Name = "grbConsulta";
+            this.grbConsulta.Size = new System.Drawing.Size(547, 291);
+            this.grbConsulta.TabIndex = 0;
+            this.grbConsulta.TabStop = false;
+            this.grbConsulta.Text = "Consulta de datos";
+            this.grbConsulta.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblCampo
+            // 
+            this.lblCampo.AutoSize = true;
+            this.lblCampo.Location = new System.Drawing.Point(40, 59);
+            this.lblCampo.Name = "lblCampo";
+            this.lblCampo.Size = new System.Drawing.Size(40, 13);
+            this.lblCampo.TabIndex = 0;
+            this.lblCampo.Text = "Campo";
+            // 
+            // lblmodo
+            // 
+            this.lblmodo.AutoSize = true;
+            this.lblmodo.Location = new System.Drawing.Point(267, 59);
+            this.lblmodo.Name = "lblmodo";
+            this.lblmodo.Size = new System.Drawing.Size(34, 13);
+            this.lblmodo.TabIndex = 1;
+            this.lblmodo.Text = "Modo";
+            // 
+            // cmbCampo
+            // 
+            this.cmbCampo.FormattingEnabled = true;
+            this.cmbCampo.Items.AddRange(new object[] {
+            "Código",
+            "Nombre",
+            "Límite",
+            "Deuda"});
+            this.cmbCampo.Location = new System.Drawing.Point(87, 55);
+            this.cmbCampo.Name = "cmbCampo";
+            this.cmbCampo.Size = new System.Drawing.Size(121, 21);
+            this.cmbCampo.TabIndex = 2;
+            // 
+            // cmbModo
+            // 
+            this.cmbModo.FormattingEnabled = true;
+            this.cmbModo.Location = new System.Drawing.Point(331, 55);
+            this.cmbModo.Name = "cmbModo";
+            this.cmbModo.Size = new System.Drawing.Size(121, 21);
+            this.cmbModo.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Usuario,
+            this.LimitedeCredito,
+            this.Deuda});
+            this.dataGridView1.Location = new System.Drawing.Point(43, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(451, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Código";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Usuario
+            // 
+            this.Usuario.HeaderText = "Usuario";
+            this.Usuario.Name = "Usuario";
+            // 
+            // LimitedeCredito
+            // 
+            this.LimitedeCredito.HeaderText = "Limite de Crédito";
+            this.LimitedeCredito.Name = "LimitedeCredito";
+            // 
+            // Deuda
+            // 
+            this.Deuda.HeaderText = "Deuda";
+            this.Deuda.Name = "Deuda";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(446, 348);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 1;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // frmListadoOrdenado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.grbConsulta);
+            this.Name = "frmListadoOrdenado";
+            this.Text = "frmListadoOrdenado";
+            this.grbConsulta.ResumeLayout(false);
+            this.grbConsulta.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbConsulta;
+        private System.Windows.Forms.Label lblmodo;
+        private System.Windows.Forms.Label lblCampo;
+        private System.Windows.Forms.ComboBox cmbModo;
+        private System.Windows.Forms.ComboBox cmbCampo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LimitedeCredito;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Deuda;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
